dc8b63925c6690b042c785dd37606a3d80f72060
mike <dq042000@gmail.com>
2022-05-04 16:06:40 +0800

    大調整

--- web/routes/web.php
+++ web/routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-    return view('index');
+Route::view('/', 'home');
+
+// // 只要是admin進來的(url: http://localhost/admin)
+// Route::prefix('admin')->group(function () {
+//     // 接在admin之後的(url: http://localhost/admin/title)
+//     Route::view('/', 'backend.title');
+//     Route::view('/title', 'backend.title');
+//     Route::view('/ad', 'backend.ad');
+// });
+
+Route::get('/admin/{module}', function ($module) {
+    switch ($module) {
+        case "title":
+            return view('backend.module', ['header' => '網站標題管理']);
+            break;
+        case "ad":
+            return view('backend.module', ['header' => '動態廣告文字管理']);
+            break;
+    }
 });
