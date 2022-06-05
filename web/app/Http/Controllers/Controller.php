386b412d4b6dc3f8ac272ca4b43248a328e78203
mike <dq042000@gmail.com>
2022-06-05 20:56:22 +0800

    edit session

--- web/app/Http/Controllers/Controller.php
+++ web/app/Http/Controllers/Controller.php
@@ -20,6 +20,16 @@ class Controller extends BaseController
     {
         $this->view['title'] = Title::where('sh', 1)->first();
         $this->view['total'] = Total::first()->total;
+        if (!session()->has('visiter')) {
+            $total = Total::first();
+            $total->total++;
+            $total->save();
+            $this->view['total'] = $total->total;
+
+            // 二種session寫法
+            // session(['visiter' => $total->total]);
+            session()->put('visiter', $total->total);
+        }
         $this->view['bottom'] = Bottom::first()->bottom;
     }
 }
